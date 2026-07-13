44ab314dcf09de45721920e6821ad40920158b9d
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-13 20:55:33 +0200

    Complete python hello world tasks

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Elmwood Institute"
+print(f"{str * 3}")
+print(f"{str[:9]}")
---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Elmwood"
+str2 = "Institute"
+print(f"Welcome to {str1} {str2}!")
