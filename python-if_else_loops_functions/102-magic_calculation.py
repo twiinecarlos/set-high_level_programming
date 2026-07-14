fc22102a40cee13c879145c5570c09ab1693e673
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-14 15:41:02 +0200

    Add magic_calculation function

---
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Performs the same calculation as the given bytecode."""
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
+
