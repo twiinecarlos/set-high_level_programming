6c14cb8714e0d88368adb7db2ad4dced585e32fa
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-14 21:56:35 +0200

    Add square comparison methods

---
+++ python-classes/102-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize a Square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the square area."""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
