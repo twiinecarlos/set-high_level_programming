2b8b29491eb94dadb217103b7f3e8653a5ae2860
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-14 18:34:11 +0200

    Implement CPython list info

---
+++ 100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints basic info about Python lists
+ * @p: Python list object
+ *
+ * Return: Nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, allocated, i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
