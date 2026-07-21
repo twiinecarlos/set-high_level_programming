8e8f3c3d404d1d9fbf5d506d689c8c6ca77788c7
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-21 12:19:21 +0200

    Add module and class documentation to LockedClass

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a LockedClass with restricted instance attributes."""
+
+
 class LockedClass:
     """Class that only allows a first_name instance attribute."""
+
     __slots__ = ["first_name"]
