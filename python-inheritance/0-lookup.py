2353c9d09a4e5ce6ddf83cd1da86e3888aedec9a
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-21 12:40:03 +0200

    Add module and function documentation to lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a lookup function for object introspection."""
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
     return dir(obj)
