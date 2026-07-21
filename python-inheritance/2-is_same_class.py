3a761bff86db10f0ab2d2159fb34fa773e1ae8d8
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-21 12:48:35 +0200

    Add module and function documentation to is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines an exact class-matching function."""
+
+
 def is_same_class(obj, a_class):
     """Returns True if obj is exactly an instance of a_class."""
     return type(obj) is a_class
