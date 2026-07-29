9121ce80f3546b862734eacb8e5e265ca2cc5cd9
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-29 23:06:00 +0200

    Add pascal_triangle function

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a function to build Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+    Pascal's triangle of n rows."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
