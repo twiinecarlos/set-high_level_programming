1dcd127c6181b5bb2f4a5f85603d17ceba6ee256
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-29 22:53:09 +0200

    Add module documentation to class_to_json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a function to convert an object to a dict
+for JSON serialization."""
+
+
 def class_to_json(obj):
     """Returns the dictionary description of an object for
     JSON serialization."""
