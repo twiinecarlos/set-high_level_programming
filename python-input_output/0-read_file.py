45f9b8b42b667aa9919284a8376d2573fc82334e
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-29 21:47:58 +0200

    Add module documentation to read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a function to read and print a text file."""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
