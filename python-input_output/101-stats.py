8d9179856350aaa18077f1156363699f64aef5a8
Twiine Carlos <twiinecarlos@gmail.com>
2026-07-29 23:59:00 +0200

    Fix stats printing to include final EOF summary

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -35,6 +35,8 @@ if __name__ == "__main__":
             if line_count % 10 == 0:
                 print_stats()
 
+        print_stats()
+
     except KeyboardInterrupt:
         print_stats()
         raise
