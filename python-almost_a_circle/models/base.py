0d7ae96ed0faefca70f28df600021bde848feb7e
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-18 21:07:48 +0200

    Add turtle-based draw method to Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -138,3 +138,45 @@ class Base:
                 return list_objs
         except IOError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Opens a window and draws all the Rectangles and Squares.
+
+        Args:
+            list_rectangles: a list of Rectangle instances to draw
+            list_squares: a list of Square instances to draw
+        """
+        import turtle
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for _ in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for _ in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
