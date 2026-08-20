d1d7ec9b04457674b6ebef4e6bd45c38837e92ff
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 00:02:02 +0200

    Fix state contains a filter

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-"""Lists State objects whose names contain the letter a."""
+"""Lists all State objects containing the letter 'a'."""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from model_state import Base, State
+
+
 if __name__ == "__main__":
- engine = create_engine(
- "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
- sys.argv[1],
- sys.argv[2],
- sys.argv[3]
- ),
- pool_pre_ping=True
- )
- Session = sessionmaker(bind=engine)
- session = Session()
- states = (
- session.query(State)
- .filter(State.name.contains("a"))
- .order_by(State.id.asc())
- .all()
- )
- for state in states:
- print("{}: {}".format(state.id, state.name))
- session.close()
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = (
+        session.query(State)
+        .filter(State.name.contains("a"))
+        .order_by(State.id.asc())
+        .all()
+    )
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
