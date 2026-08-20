9808187c28478471c4dd5496973dfc92878aee58
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 00:43:19 +0200

    Add relationship state city list script

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
-"""Lists states and their related cities using one query."""
+"""Lists all states and their related cities."""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import joinedload, sessionmaker
+
 from relationship_city import City
-from relationship_state import Base, State
+from relationship_state import State
+
+
 if __name__ == "__main__":
- engine = create_engine(
- "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
- sys.argv[1],
- sys.argv[2],
- sys.argv[3]
- ),
- pool_pre_ping=True
- )
- Session = sessionmaker(bind=engine)
- session = Session()
- states = (
- session.query(State)
- .options(joinedload(State.cities))
- .order_by(State.id.asc())
- .all()
- )
- for state in states:
- print("{}: {}".format(state.id, state.name))
- for city in sorted(state.cities, key=lambda city: city.id):
- print("\t{}: {}".format(city.id, city.name))
- session.close()
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id.asc())
+        .all()
+    )
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in sorted(state.cities, key=lambda city: city.id):
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
