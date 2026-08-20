d92d55e10ba495cd2d0d0d04243e9b54395be3fd
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-20 23:55:21 +0200

    Fix first state fetch script

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """Prints the first State object from the database."""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from model_state import Base, State
+
+
 if __name__ == "__main__":
- engine = create_engine(
- "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
- sys.argv[1],
- sys.argv[2],
- sys.argv[3]
- ),
- pool_pre_ping=True
- )
- Session = sessionmaker(bind=engine)
- session = Session()
- state = session.query(State).order_by(State.id.asc()).first()
- if state is None:
- print("Nothing")
- else:
- print("{}: {}".format(state.id, state.name))
- session.close()
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id.asc()).first()
+
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
