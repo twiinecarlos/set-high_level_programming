30bd36037c689ef7f6533e32d748ef13e6a9f310
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-20 23:31:07 +0200

    Fix State model and add table creation script

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Defines the State class and SQLAlchemy Base."""
+"""Defines the State class and Base instance."""
+
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
+
 Base = declarative_base()
+
+
 class State(Base):
- """Represents the states table."""
- __tablename__ = "states"
- id = Column(
- Integer,
- primary_key=True,
- nullable=False,
- autoincrement=True
- )
- name = Column(String(128), nullable=False)
+    """Represents the states table."""
+
+    __tablename__ = "states"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    name = Column(String(128), nullable=False)
