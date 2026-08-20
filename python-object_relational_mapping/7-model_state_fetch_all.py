3fc3c6a18d52946ab29defe4d1476e0f50fe44a0
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-20 23:45:46 +0200

    Fix SQLAlchemy state list script

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """Lists all State objects from the database."""
+
 import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from model_state import Base, State
+
+
 if __name__ == "__main__":
- engine = create_engine(
- "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
- sys.argv[1],
- sys.argv[2],
- sys.argv[3]
- ),
- pool_pre_ping=True
- )
- Session = sessionmaker(bind=engine)
- session = Session()
- states = session.query(State).order_by(State.id.asc()).all()
- for state in states:
- print("{}: {}".format(state.id, state.name))
- session.close()
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
