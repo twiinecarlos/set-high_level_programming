c61d35f8807fa939317b4c77c81f401a561ba35f
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 21:04:22 +0200

    Add urllib status script

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Fetches the status of the ALX intranet using urllib."""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+        "https://alx-intranet.hbtn.io/status"
+    ) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
