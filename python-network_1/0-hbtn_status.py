78adaddbb7b69bbe2a7c4bf271c7bdd10cbadbf3
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 21:17:11 +0200

    Fix status URL fallback

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""Fetches the status of the ALX intranet using urllib."""
+"""Fetches the status of the intranet using urllib."""
 
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(
-        "https://alx-intranet.hbtn.io/status"
-    ) as response:
-        body = response.read()
+    urls = [
+        "https://alx-intranet.hbtn.io/status",
+        "https://intranet.hbtn.io/status"
+    ]
+
+    body = None
+
+    for url in urls:
+        try:
+            with urllib.request.urlopen(url) as response:
+                body = response.read()
+            break
+        except Exception:
+            pass
 
     print("Body response:")
     print("\t- type: {}".format(type(body)))
