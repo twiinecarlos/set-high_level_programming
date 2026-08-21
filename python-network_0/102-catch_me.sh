eafa77765c0f9a0b19d3d01300fd262dbe21d8a4
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 20:57:01 +0200

    Fix catch me curl script

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Makes the required request and follows redirects with user_id 98.
-curl -sL -X PUT -H "user_id: 98" 0.0.0.0:5000/catch_me
+# Makes the catch_me request with the required user ID header.
+curl -sL -X PUT -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me
