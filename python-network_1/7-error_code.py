ad0c77c12aa80c53dff0cce73d0fd293762cb273
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 21:50:42 +0200

    Add requests error handling script

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Displays a response body or HTTP error code using requests."""
+
+import sys
+
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
