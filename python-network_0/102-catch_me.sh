2f44387fc1ea62bc9593878bab6c964ce92c5d8a
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 20:47:34 +0200

    Fix catch me redirect

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Makes the required request to catch the server and display its response.
-curl -sX PUT -H "Origin: School" 0.0.0.0:5000/catch_me
+# Makes the required request and follows redirects to display the final response.
+curl -sL -X PUT -H "Origin: School" 0.0.0.0:5000/catch_me
