33a4696a06252eadf747ad7730cf5c5060ee1639
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 20:24:28 +0200

    Fix POST parameters script

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Sends a POST request with the required email and subject parameters.
-curl -sX POST -d "email=test@gmail.com" \
-    -d "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
