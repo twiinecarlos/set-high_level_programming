3cbfbf0cc15a0d0f6dccc62f827a18115dd5a98e
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 21:37:45 +0200

    Fix requests status script

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""Fetches the status of the ALX intranet using requests."""
+"""Fetches the intranet status using requests."""
 
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/status")
+    try:
+        response = requests.get(
+            "https://alx-intranet.hbtn.io/status"
+        )
+    except requests.RequestException:
+        response = requests.get(
+            "https://intranet.hbtn.io/status"
+        )
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
