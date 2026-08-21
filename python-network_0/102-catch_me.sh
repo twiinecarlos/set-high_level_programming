44e24ebc260b800b3162968394ec3bc7a9e6570f
Twiine Carlos <twiinecarlos@gmail.com>
2026-08-21 20:50:52 +0200

    Fix catch me user id header

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Makes the required request and follows redirects to display the final response.
-curl -sL -X PUT -H "Origin: School" 0.0.0.0:5000/catch_me
+# Makes the required request and follows redirects with user_id 98.
+curl -sL -X PUT -H "user_id: 98" 0.0.0.0:5000/catch_me
